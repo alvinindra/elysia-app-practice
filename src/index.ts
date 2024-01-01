6d9ecfe8137f93a4e9b4947a955e39c90469acb5
Alvin <alvinindrap@gmail.com>
2024-01-01 12:10:45 +0700

    initial commit elysia

--- src/index.ts
+++ src/index.ts
@@ -54,6 +54,7 @@ const app = new Elysia()
 })
 .listen(3000);
 
+console.log('=== Elysia is running!!! ===')
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
